ba58337d07dca0e11721bcf186f7c20779df04cd
Joachim Meyer <j.meyer@utah.edu>
2020-03-05 12:02:42 -0700

    Fix gcc compiler version for conda activate env.

--- conda/isnobal-activate.sh
+++ conda/isnobal-activate.sh
@@ -7,7 +7,7 @@ export MACPORTS=/Volumes/warehouse/projects/macports/bin
 
 export PATH=${IPW_PATH}/bin:${IPW_PATH}/sbin:${MACPORTS}:$PATH
 
-export CC="gcc-9"
+export CC="gcc-7"
 #export LDFLAGS="-L${CONDA_PREFIX}/include -Wl,-rpath,${CONDA_PREFIX}/include"
 #export CPPFLAGS="-I${CONDA_PREFIX}/include"
 
