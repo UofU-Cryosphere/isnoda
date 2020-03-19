c54dec105f67bf4020064356203cf86528691ba0
Joachim Meyer <j.meyer@utah.edu>
2020-03-19 17:48:17 -0600

    Conda activate script - Remove MacPorts env.
    MacPorts is specific to installing wgrib2 and not all environments
    will need that. Also comment out CC variable.

--- conda/isnobal-activate.sh
+++ conda/isnobal-activate.sh
@@ -4,11 +4,9 @@ IPW_PATH=$HOME/isnobal/ipw
 export IPW=$IPW_PATH
 export WORKDIR=$HOME/isnobal
 
-export MACPORTS=/Volumes/warehouse/projects/macports/bin
+export PATH=${IPW_PATH}/bin:${IPW_PATH}/sbin:$PATH
 
-export PATH=${IPW_PATH}/bin:${IPW_PATH}/sbin:${MACPORTS}:$PATH
-
-export CC="gcc-7"
+#export CC="gcc-7"
 #export LDFLAGS="-L${CONDA_PREFIX}/include -Wl,-rpath,${CONDA_PREFIX}/include"
 #export CPPFLAGS="-I${CONDA_PREFIX}/include"
 
--- conda/isnobal-deactivate.sh
+++ conda/isnobal-deactivate.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-for ISNOBAL_PATH in "${IPW_PATH}/sbin" "${IPW_PATH}/bin" "${MACPORTS}" ; do
+for ISNOBAL_PATH in "${IPW_PATH}/sbin" "${IPW_PATH}/bin"; do
   PATH="${PATH//$ISNOBAL_PATH:}"
 done
 
 unset IPW
 unset IPW_PATH
 unset WORKDIR
-
-unset MACPORTS
 
 unset CC
 #unset LDFLAGS
