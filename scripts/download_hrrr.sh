4879e87f2ea5b7c32fc0465f94557b0cd201a507
Joachim Meyer <j.meyer@utah.edu>
2020-08-12 13:31:22 -0600

    HRRR download - Turn down verbosity for commands.

--- scripts/download_hrrr.sh
+++ scripts/download_hrrr.sh
@@ -16,13 +16,13 @@ for DATE in "${DATES[@]}"; do
         FILE_NAME="hrrr.t$(printf "%02d" $HOUR)z.wrfsfcf0${FIELD}.grib2"
 
         grib_download="${FILE_NAME}_download"
-        wget --no-check-certificate "https://pando-rgw01.chpc.utah.edu/hrrr/sfc/${DATE}/${FILE_NAME}" -O $grib_download
+        wget -nv --no-check-certificate "https://pando-rgw01.chpc.utah.edu/hrrr/sfc/${DATE}/${FILE_NAME}" -O $grib_download
 
         wgrib_file="${FILE_NAME}_tmp"
         mkfifo $wgrib_file
 
-        wgrib2 -ncpu 8 $grib_download -small_grib -109.06:-102 36.99:41.01 $wgrib_file &
-        wgrib2 $wgrib_file -ncpu 8 -match "$HRRR_VARS" -GRIB $FILE_NAME
+        wgrib2 -v0 -ncpu 8 $grib_download -small_grib -109.06:-102 36.99:41.01 $wgrib_file &
+        wgrib2 -v0 -ncpu 8 $wgrib_file -match "$HRRR_VARS" -GRIB $FILE_NAME
 
         rm $wgrib_file
         rm $grib_download
