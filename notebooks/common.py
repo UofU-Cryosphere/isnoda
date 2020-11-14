a5ad14368aeb2af2845ebc52c8a6cfd4bbab3fce
Joachim Meyer <j.meyer@utah.edu>
2020-11-14 16:48:57 -0700

    Notebooks - Create common.py for shared elements.

---
+++ notebooks/common.py
@@ -0,0 +1,13 @@
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+from pathlib import PurePath, Path
+
+plt.rcParams.update(
+    {
+        'axes.labelsize': 14
+    }
+)
