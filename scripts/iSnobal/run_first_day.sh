27f1b53f149dc0cf3d443a8560f8b3a3ead612a1
Joachim Meyer <j.meyer@utah.edu>
2021-01-22 13:06:32 -0700

    Scripts - Add wrapper to run first day of a water year.
    Currently hard coded to a specific .ini file.

---
+++ scripts/iSnobal/run_first_day.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# Run for given day. Date format: 2017-10-01 00:00
+
+source load_conda_env isnoda
+
+# Initial run, no previous days
+awsm_daily_airflow -c ${HOME}/project-data/iSnobal/ERW/ERW_subset_awsm.ini \
+  --no_previous \
+  --start_date $@
