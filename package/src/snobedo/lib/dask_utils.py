9823b3f90e047f4e35b422e336eb1671a06f56bf
Joachim Meyer <j.meyer@utah.edu>
2021-01-28 11:33:12 -0700

    Snobedo - Add lib
    Start lib folder containing shared helpers. Kick off with dask_cluster
    and a MODIS GeoTiff template for the Western US.

---
+++ package/src/snobedo/lib/dask_utils.py
@@ -0,0 +1,31 @@
+from dask.distributed import Client
+from pathlib import Path
+
+
+CHPC = 'chpc' in str(Path.home())
+
+
+def start_cluster(cores=6, memory=None):
+    if CHPC:
+        from dask_jobqueue import SLURMCluster
+
+        cluster = SLURMCluster(
+            cores=cores,
+            processes=cores,
+            project="notchpeak-shared-short",
+            queue="notchpeak-shared-short",
+            memory=f"{memory or cores}g",
+            walltime="2:00:00",
+        )
+
+        cluster.scale(1)
+        client = Client(cluster)
+    else:
+        # Assume local
+        from dask.distributed import LocalCluster
+
+        cluster = LocalCluster(memory_limit='1.5GB')
+        client = Client(cluster)
+
+    # print(cluster.job_script())
+    return client
