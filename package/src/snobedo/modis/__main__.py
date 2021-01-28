fc90e58f241c932ee6a1c1bf91e04c402a08aede
Joachim Meyer <j.meyer@utah.edu>
2021-01-28 12:38:47 -0700

    Snobedo - Move script logic to extract MODIS albedo to package.
    Add logic to read a specific variable from a MODIS matlab file and
    stores it in a Zarr file.

---
+++ package/src/snobedo/modis/__main__.py
@@ -0,0 +1,108 @@
+import argparse
+from datetime import datetime, timedelta
+from pathlib import Path
+from typing import NamedTuple
+
+import numpy as np
+from osgeo import osr
+
+from snobedo.lib import ModisGeoTiff
+from snobedo.modis.geotiff_to_zarr import write_zarr
+from snobedo.modis.matlab_to_geotiff import matlab_to_geotiff, warp_to
+
+ONE_DAY = timedelta(days=1)
+
+
+class ConversionConfig(NamedTuple):
+    variable: str
+    source_dir: Path
+    output_dir: Path
+    modis_us: ModisGeoTiff
+    target_srs: osr.SpatialReference
+
+
+def argument_parser():
+    parser = argparse.ArgumentParser(
+        description='Convert matlab files to zarr',
+    )
+
+    parser.add_argument(
+        '--source-dir',
+        required=True,
+        type=Path,
+        help='Base directory. The files to convert are expected to be in a '
+             'folder with the water year. Example: 2018'
+             ' Other required file expected under this folder is the template '
+             f'MODIS file with name: {ModisGeoTiff.WESTERN_US_TEMPLATE}',
+    )
+    parser.add_argument(
+        '--water-year',
+        required=True,
+        type=int,
+        help='Water year. Determines the date range to process'
+    )
+    parser.add_argument(
+        '--variable',
+        required=True,
+        type=str,
+        help='Variable to extract from the matlab files'
+    )
+    parser.add_argument(
+        '--t-srs',
+        type=int,
+        default=32613,
+        help='Target EPSG. Default: EPSG:32613'
+    )
+
+    return parser
+
+
+def config_for_arguments(arguments):
+    target_srs = osr.SpatialReference()
+    target_srs.ImportFromEPSG(arguments.t_srs)
+
+    output_dir = arguments.source_dir / f'wy{arguments.water_year}-zarr/'
+    output_dir.mkdir(exist_ok=True)
+
+    return ConversionConfig(
+        variable=arguments.variable,
+        source_dir=arguments.source_dir,
+        output_dir=output_dir,
+        modis_us=ModisGeoTiff(arguments.source_dir),
+        target_srs=target_srs
+    )
+
+
+def date_range(water_year):
+    d0 = datetime(water_year, 9, 1)
+    d1 = datetime(water_year + 1, 10, 1)
+
+    return np.arange(d0, d1, ONE_DAY).astype(datetime)
+
+
+def write_date(date, config):
+    file = matlab_to_geotiff(
+        config.source_dir,
+        config.output_dir,
+        config.modis_us,
+        date,
+        config.variable,
+    )
+    file = warp_to(file, config.target_srs)
+    write_zarr(file, date, config.variable, config.output_dir)
+
+
+def main():
+    arguments = argument_parser().parse_args()
+
+    if not arguments.source_dir.exists():
+        raise IOError(
+            f'Given source folder does not exist: {arguments.source_dir}'
+        )
+
+    config = config_for_arguments(arguments)
+    [write_date(date, config) for date in date_range(arguments.water_year)]
+
+
+if __name__ == '__main__':
+    main()
---
+++ package/src/snobedo/modis/matlab_to_geotiff.py
@@ -0,0 +1,73 @@
+import mat73
+from osgeo import gdal, gdalconst, gdalnumeric
+
+from snobedo.lib import ModisGeoTiff
+
+MODIS_MATLAB_FILE = "westernUS_Terra_{date:%Y%m%d}_mindays10_minthresh05_" \
+                   "ndsimin0.00_zthresh08000800.mat"
+
+OUTPUT_FILE = 'WesternUS_{date:%Y%m%d}_{key}'
+
+GDAL_GTIFF = 'GTiff'
+GDAL_VRT = 'VRT'
+GTIFF_DRIVER = gdal.GetDriverByName(GDAL_GTIFF)
+GTIFF_DRIVER_OPTS = [
+    "COMPRESS=LZW",
+    "TILED=YES",
+    "BIGTIFF=IF_SAFER",
+    "NUM_THREADS=ALL_CPUS"
+]
+GTIFF_FILE = OUTPUT_FILE + '.tif'
+
+BAND_DATA_TYPE = gdalconst.GDT_UInt16
+BAND_NO_DATA_VALUE = 65535
+BAND_NUMBER = 1
+
+
+def matlab_file(data_dir, date):
+    return data_dir.joinpath(
+        '{date:%Y}', MODIS_MATLAB_FILE
+    ).as_posix().format(date=date)
+
+
+def matlab_to_geotiff(source_dir, output_dir, template_file, date, variable):
+    file_name = output_dir.joinpath(
+        GTIFF_FILE.format(date=date, key=variable)
+    ).as_posix()
+
+    geo_tiff = GTIFF_DRIVER.Create(
+        file_name,
+        template_file.x_size, template_file.y_size,
+        BAND_NUMBER, BAND_DATA_TYPE,
+        options=GTIFF_DRIVER_OPTS,
+    )
+    geo_tiff.SetGeoTransform(template_file.geo_transform)
+    geo_tiff.SetProjection(ModisGeoTiff.PROJECTION)
+
+    modis_band = geo_tiff.GetRasterBand(BAND_NUMBER)
+    modis_band.SetNoDataValue(BAND_NO_DATA_VALUE)
+
+    gdalnumeric.BandWriteArray(
+        modis_band,
+        mat73.loadmat(matlab_file(source_dir, date))[variable]
+    )
+
+    modis_band.ComputeStatistics(0)
+    modis_band.FlushCache()
+
+    del modis_band
+    del geo_tiff
+
+    return file_name
+
+
+def warp_to(file_name, target_srs):
+    vrt_file = file_name.replace(
+        '.tif', f'_{target_srs.GetAuthorityCode(None)}.vrt'
+    )
+    gdal.Warp(
+        vrt_file, file_name,
+        dstSRS=target_srs, resampleAlg=gdalconst.GRIORA_Bilinear,
+        multithread=True, format=GDAL_VRT,
+    )
+    return vrt_file
