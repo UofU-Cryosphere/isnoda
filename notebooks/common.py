87c2f89d9719ce9810f73de0b142fb499463232b
Joachim Meyer <j.meyer@utah.edu>
2021-03-09 09:14:14 -0700

    Notebooks - common - Add helper for a legend box.
    Add matplotlib helper to add a legend box with statistics to a plot.

--- notebooks/common.py
+++ notebooks/common.py
@@ -1,16 +1,36 @@
 import datetime
-from pathlib import Path, PurePath
-
-from snobedo.lib.dask_utils import start_cluster
-
-import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import xarray as xr
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.patches as mpatches
+import matplotlib.font_manager as font_manager
+
+from pathlib import Path, PurePath
+
+from snobedo.lib.dask_utils import start_cluster
 
 plt.rcParams.update(
     {
         'axes.labelsize': 10
     }
 )
+
+LEGEND_TEXT = "{0:10} {1:8}"
+LEGEND_DATE = "%Y-%m-%d"
+
+def legend_text(label, value, color='none'):
+    return mpatches.Patch(
+        color=color, label=LEGEND_TEXT.format(label, value)
+    )
+
+def add_legend_box(ax, entries):
+    ax.legend(
+        handles=entries,
+        loc='upper right',
+        prop=font_manager.FontProperties(
+            family='monospace', style='normal', size=8
+        ),
+    )
