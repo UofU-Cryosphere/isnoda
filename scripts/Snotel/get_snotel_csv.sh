f71525815d16827fbeb0a7b6c2c1c586700dacbc
Joachim Meyer <j.meyer@utah.edu>
2021-03-30 10:47:55 -0600

    Scripts - Snotel - Fix CSV download destination file name.

--- scripts/Snotel/get_snotel_csv.sh
+++ scripts/Snotel/get_snotel_csv.sh
@@ -19,4 +19,4 @@
 HOST="https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customMultiTimeSeriesGroupByStationReport,metric/daily/end_of_period/"
 COLUMNS="/SNWD::value,WTEQ::value,SNDN::value,WSPDV::value,TOBS::value,PRCP::value"
 
-curl "${HOST}${1}:CO:SNTL%7Cid=%22%22%7Cname/$(($2 - 1))-10-01,${2}-09-30${COLUMNS}" > ${2}-{1}.csv
+curl "${HOST}${1}:CO:SNTL%7Cid=%22%22%7Cname/$(($2 - 1))-10-01,${2}-09-30${COLUMNS}" > ${2}-${1}.csv
