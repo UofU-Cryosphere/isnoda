4ea41f163897bc0c776854dfdcbe1f9817ec0e07
Joachim Meyer <j.meyer@utah.edu>
2021-03-30 10:35:57 -0600

    Scripts - Snotel - Add precipitation to csv download.

--- scripts/Snotel/get_snotel_csv.sh
+++ scripts/Snotel/get_snotel_csv.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #
 # Download Snotel csv data from USDA for a site with end of day values in
-# metric units. Includes: Depth, SWE, Density, Wind Speed, Air Temperature
+# metric units.
+# Data includes: Depth, SWE, Density, Wind Speed, Air Temperature, Precipitation
+#
 # More information is in the header of the generated csv file.
 #
 # Arguments:
@@ -12,9 +14,9 @@
 #   Name: WaterYear-SiteID.csv
 #
 # Example:
-# get_snotel_csv.sh 111 2020
+#  get_snotel_csv.sh 111 2020
 
 HOST="https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customMultiTimeSeriesGroupByStationReport,metric/daily/end_of_period/"
-COLUMNS="/SNWD::value,WTEQ::value,SNDN::value,WSPDV::value,TOBS::value"
+COLUMNS="/SNWD::value,WTEQ::value,SNDN::value,WSPDV::value,TOBS::value,PRCP::value"
 
 curl "${HOST}${1}:CO:SNTL%7Cid=%22%22%7Cname/$(($2 - 1))-10-01,${2}-09-30${COLUMNS}" > ${2}-{1}.csv
