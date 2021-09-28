d4d6001fbd930edd33a63c5703868871e912a7d5
Joachim Meyer <j.meyer@utah.edu>
2021-09-28 08:47:29 -0600

    Scripts - Katana - Update execution wrapper for SLURM.

--- scripts/katana/katana_singularity.sh
+++ scripts/katana/katana_singularity.sh
@@ -2,12 +2,17 @@
 # Convert docker to singularity
 #  singularity build ars-nwrc-katana.sif docker://usdaarsnwrc/katana
 #
-# - ${1}: Directory with Topo
-# - ${2}: HRRR input data home
+# - ${1}: Basin setup directory with topo
+# - ${2}: Path to HRRR input data
 # - ${3}: Output folder for SMRF to ingest it
+
+## Update these two variables with actual paths ##
+KATANA_IMAGE=/path/to/katana.sif
+KATANA_INI=/path/to/katana.ini
+## ------------------------------ ##
 
 ml singularity
 
 export SINGULARITY_BIND="${1}:/data/topo,${2}:/data/input,${3}:/data/output"
 
-singularity exec ars-nwrc-katana.sif run_katana katana.ini
+singularity exec ${KATANA_IMAGE} run_katana ${KATANA_INI}
