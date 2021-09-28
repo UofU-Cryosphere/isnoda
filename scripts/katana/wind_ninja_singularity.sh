54b93d231e31a2d8ea76f2f8b4b9904ab111b295
Joachim Meyer <j.meyer@utah.edu>
2021-09-28 08:52:00 -0600

    Scripts - Katana - Update WindNinja wrapper.

--- scripts/katana/wind_ninja_singularity.sh
+++ scripts/katana/wind_ninja_singularity.sh
@@ -6,9 +6,12 @@
 # - ${2}: HRRR input data home
 # - ${3}: Output folder for SMRF to ingest it
 
+## Update the variable with the actual path ##
+KATANA_IMAGE=/path/to/katana.sif
+## ------------------------------ ##
+
 ml singularity
 
 export SINGULARITY_BIND="${1}:/data/topo,${2}:/data/input,${3}:/data/output"
 
-singularity exec ${HOME}/project-data/iSnobal/katana/katana_jm.sif \
-  WindNinja_cli /data/output/katana.ini
+singularity exec ${KATANA_IMAGE} WindNinja_cli /data/output/katana.ini
