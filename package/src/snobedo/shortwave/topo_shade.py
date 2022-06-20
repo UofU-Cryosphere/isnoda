414a8de8c02e02774d9f92276441406b6f1d5fa1
Joachim Meyer <j.meyer@utah.edu>
2022-06-20 17:19:10 -0600

    Snobedo - TopoShade - Add store azimuth and zenith angle
    Save the two solar angles for comparison with an alternative calculation
    method. To be introduced soon ...

--- package/src/snobedo/shortwave/topo_shade.py
+++ package/src/snobedo/shortwave/topo_shade.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from topocalc.shade import shade
 
@@ -9,11 +11,21 @@ from snobedo.shortwave.sunang import sunang
 class TopoShade:
     def __init__(self, topo_file_path):
         self._illumination_angels = {}
+        self._azimuth = {}
+        self._zenith = {}
         self._topo_file = topo_file_path
+
+    @property
+    def azimuth(self):
+        return self._azimuth
 
     @property
     def illumination_angles(self):
         return self._illumination_angels
+
+    @property
+    def zenith(self):
+        return self._zenith
 
     @property
     def topo_file(self):
@@ -28,38 +40,62 @@ class TopoShade:
 
         for timestep in time_range:
             timestep = timestep
-            cosz, azimuth, rad_vec = sunang(
+            cosine_zenith, azimuth, rad_vec = sunang(
                 timestep,
                 topo.lat,
                 topo.lon
             )
 
-            if cosz > 0:
+            if cosine_zenith > 0:
                 illumination_angle = shade(
                     topo.sin_slope,
                     topo.aspect,
                     azimuth,
-                    cosz
+                    cosine_zenith
                 )
             else:
                 illumination_angle = np.zeros(topo.x.shape)
+                azimuth = 0
+                cosine_zenith = 1
 
+            self.azimuth[timestep] = azimuth
+            self.zenith[timestep] = math.degrees(math.acos(cosine_zenith))
             self.illumination_angles[timestep] = illumination_angle
+
+    @staticmethod
+    def add_illumination_angle(outfile):
+        field = outfile.createVariable(
+            'illumination_angle', 'f8', ('time', 'y', 'x',), zlib=True
+        )
+        field.setncattr('long_name', 'Local illumination angle')
+        field.setncattr(
+            'description',
+            'Cosine of the local illumination angle over a DEM'
+        )
+        field.setncattr(
+            'units', '1: no illumination; 0: full illumination'
+        )
+        return field
+
+    @staticmethod
+    def add_time_variable(name, description, units, outfile):
+        field = outfile.createVariable(name, 'f8', 'time', zlib=True)
+        field.setncattr('long_name', description)
+        field.setncattr('units', units)
+        return field
 
     def save_illumination_angles(self, out_file_path):
         time_range = list(self.illumination_angles.keys())
 
         with WriteNC.for_topo(out_file_path, self.topo_file) as outfile:
-            field = outfile.createVariable(
-                'illumination_angle', 'f8', ('time', 'y', 'x',), zlib=True
+            illumination_field = self.add_illumination_angle(outfile)
+            azimuth_field = self.add_time_variable(
+                'azimuth', 'Solar azimuth angle; 0 -> South', 'degrees',
+                outfile
             )
-            field.setncattr('long_name', 'Local illumination angle')
-            field.setncattr(
-                'description',
-                'Cosine of the local illumination angle over a DEM'
-            )
-            field.setncattr(
-                'units', '1: no illumination; 0: full illumination'
+            zenith_field = self.add_time_variable(
+                'zenith', 'Solar zenith angle; 90 -> Horizon', 'degrees',
+                outfile
             )
 
             counter = 0
@@ -68,5 +104,8 @@ class TopoShade:
                 outfile['time'][counter] = WriteNC.date_to_number(
                     key, outfile, counter == 0
                 )
-                field[counter, :, :] = self.illumination_angles[key]
+                illumination_field[counter, :, :] = \
+                    self.illumination_angles[key]
+                azimuth_field[counter] = self.azimuth[key]
+                zenith_field[counter] = self.zenith[key]
                 counter += 1
