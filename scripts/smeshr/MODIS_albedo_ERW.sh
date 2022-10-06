b1b23e4aacab56e78da1ca835c945747ec1a50fe
Joachim Meyer <j.meyer@utah.edu>
2022-10-06 16:23:03 -0600

    Scripts - SMESHR - Try cubic resampling for MODIS albedo
    Updates the script to extract albedo value for ERW and uses the cubic
    resampling when scaling to 50m output resolution. This is an experiment
    to reduce artifacts due to nearest neighbor resampling that showed MODIS
    pixel outlines in the snow depth.

--- scripts/smeshr/MODIS_albedo_ERW.sh
+++ scripts/smeshr/MODIS_albedo_ERW.sh
@@ -1,56 +1,76 @@
 #!/usr/bin/env bash
+
 # Script to extract albedo values for ERW model domain from MODIS.
-# This prepares a NetCDF file with per grid cell albedo values. It can be
-# directly used to calculate net solar with incoming shortwave radiation.
+# This prepares a NetCDF file with per grid cell albedo values for every
+# hour of one day. It can be directly used to calculate net solar 
+# with incoming shortwave radiation.
 
 # Arguments:
 # ${1}: /path/to/MODIS/inputs
 # ${2}: water year
-# ${3}: /path/to/extracted/MODIS/albedo
 
-# command from isnoda snobedo package
+# command from UofU snow-rs package
+# https://github.com/UofU-Cryosphere/snow-rs
+# Converts the MODIS matlab to GeoTiff files
 variable_from_modis --source-dir ${1} \
                     --water-year ${2} \
                     --variable albedo_observed_muZ
 
+if [ $? != 0 ]; then
+  echo "Error extracting variable from MODIS inputs"
+  exit 1
+fi
+
 export ERW_NC="_ERW.nc"
 export ERW_ONE_DAY_SUFFIX="_24.nc"
 
-# First argument is the tif; second the boundaries
-modis_erw() {
-  ERW_TMP=${1/\.tif/_tmp.nc}
+export CDO_call="cdo -O -z zip_4 -s"
 
-  gdalwarp -overwrite -multi\
-  -co FORMAT=NC4C -co WRITE_BOTTOMUP=NO -ot Float32 \
-  -t_srs EPSG:32613 -tr 50 50 -dstnodata -1\
-  -te 315900.0 4280850.0 348700.0 4322700.0 \
-  ${1} ${ERW_TMP}
+# Extracts the ERW domain from the Western US MODIS albedo GeoTiffs
+modis_erw() {
+  ERW_TMP_VRT=${1/\.tif/_tmp.vrt}
+  ERW_TMP_NC=${1/\.tif/_tmp.nc}
+
+  gdalwarp -q -overwrite -multi\
+    -r cubic \
+    -t_srs EPSG:32613 -tr 50 50 -dstnodata 65535 \
+    -te 315900.0 4280850.0 348700.0 4322700.0 \
+    ${1} ${ERW_TMP_VRT}
+
+  # There are negative resampling values when going
+  # straight from MODIS GeoTiff to ERW NetCDF. Breaking
+  # up in two steps via VRT
+  gdalwarp -overwrite -multi -q \
+    -co FORMAT=NC4C -co WRITE_BOTTOMUP=NO \
+    ${ERW_TMP_VRT} ${ERW_TMP_NC}
 
   if [ $? != 0 ]; then
     printf "Error extracting domain from: \n   ${1}\n"
     return 1
   fi
 
-  date=$(date -d $(echo $1 | cut -d '_' -f2) +%Y-%m-%d)
+  date=$(date -d $(basename $1 | cut -d '_' -f2) +%Y-%m-%d)
   # Set time and variable name in NetCDF
-  cdo -O -z zip_4 -chname,Band1,albedo \
-                  -setdate,"${date}" \
-                  ${ERW_TMP} ${1/\.tif/${ERW_NC}}
+  ${CDO_call} \
+    -expr,"Band1=((Band1 > 10000)) ? 10000 : Band1" \
+    -setdate,"${date}" \
+    ${ERW_TMP_NC} ${1/\.tif/${ERW_NC}}
 
   if [ $? != 0 ]; then
     echo "Error setting NetCDF variable name and time"
     return 1
   fi
 
-  rm ${ERW_TMP}
-  rm ${1}
+  rm ${ERW_TMP_VRT}
+  rm ${ERW_TMP_NC}
 }
 export -f modis_erw
-parallel --tag --line-buffer --jobs ${SLURM_NTASKS} modis_erw ::: ${3}
+parallel --tag --line-buffer --jobs ${SLURM_NTASKS} modis_erw ::: ${1}/wy${2}/*.tif
 
+# Creates a NetCDF using the extracted ERW domain values and
+# populates every hour of one day with those.
 albedo_day() {
-  HOUR_FILE="shift_${1}"
-  HOUR_FILE=${HOUR_FILE/\.nc$//}
+  HOUR_FILE=${1/\.nc$//}
 
   if [[ ! -f "${1}" ]]; then
     printf "Error: Source NetCDF:  \n  ${1}\n  does not exist\n"
@@ -58,19 +78,24 @@ albedo_day() {
   fi
 
   for hour in {1..23}; do
-    cdo -O -z zip_4 -shifttime,${hour}hour $1 ${HOUR_FILE}_${hour}.nc
+    ${CDO_call} -shifttime,${hour}hour \
+      ${1} ${HOUR_FILE}_${hour}.nc
   done
 
-  cdo -O -z zip_4 mergetime $1 ${HOUR_FILE}*.nc ${1/\.nc/${ERW_ONE_DAY_SUFFIX}}
+  ${CDO_call} mergetime -chname,Band1,albedo $1 \
+    ${HOUR_FILE}*.nc ${1/\.nc/${ERW_ONE_DAY_SUFFIX}}
 
   if [ $? != 0 ]; then
     printf "Error: Could not merge hours for:\n  ${1}\n"
     return 1
   fi
 
+  printf "*\n"
+
   rm ${HOUR_FILE}*.nc
   rm ${1}
 }
 
 export -f albedo_day
-parallel --tag --line-buffer --jobs ${SLURM_NTASKS} albedo_day ::: *${ERW_NC}
+parallel --tag --line-buffer --jobs ${SLURM_NTASKS} albedo_day ::: ${1}/wy${2}/*${ERW_NC}
+
