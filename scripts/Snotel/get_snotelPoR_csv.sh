ff204bdb923c5a9684ec38a776a0fcfaea067ebb
J. Michelle Hu <43484205+jmichellehu@users.noreply.github.com>
2024-07-23 18:18:31 -0600

    scrtips - Snotel - add help, default state, argument count

--- scripts/Snotel/get_snotelPoR_csv.sh
+++ scripts/Snotel/get_snotelPoR_csv.sh
@@ -16,11 +16,38 @@
 # Example:
 #  get_snotelPoR_csv.sh 112
 
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo 'Provide site number as the sole argument'
+    exit 1
+fi
+
+
+Help()
+{
+   # Display Help
+   echo "Download SNOTEL period of record data for input site"
+   echo
+   echo "Syntax: get_snotelPoR_csv.sh siteID [-h]"
+   echo "options:"
+   echo "h     Print this Help."
+   echo
+}
+
+# Get the options
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+   esac
+done
+
 HOST="https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customMultiTimeSeriesGroupByStationReport,metric/daily/end_of_period/"
 COLUMNS="/POR_BEGIN,POR_END/SNWD::value,WTEQ::value,SNDN::value,WSPDV::value,TOBS::value,PRCP::value"
-STATE="CO"
+STATE="${VARIABLE:-CO}"
 
 echo "Downloading SNOTEL period of record data for site ${1}"
 curl "${HOST}${1}:${STATE}:SNTL%7Cid=%22%22%7Cname/${COLUMNS}" > $(date +%Y%m%d)_PoR_site${1}.csv
-
 
