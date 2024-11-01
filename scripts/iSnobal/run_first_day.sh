acd74b1afa3239dfd0dca057c88bb24d58218627
J. Michelle Hu <43484205+jmichellehu@users.noreply.github.com>
2024-11-01 16:45:27 -0600

    scripts - iSnobal - generalize and reformat
    Checks active conda env for quick error outs. Adjust config
    file input and removes load_conda_env, which is sourced from
    separate repo.

--- scripts/iSnobal/run_first_day.sh
+++ scripts/iSnobal/run_first_day.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
+
 # Run iSnobal for the first day of the simulation
 # Usage: run_first_day.sh <config_file> <start_date>
 # Example: run_first_day.sh /path/to/awsm.ini 2017-10-01
 
 awsm_ini=$1
 start=$2
+
+# Run for given day. Date format: 2017-10-01 00:00
+if [[ -n "$CONDA_DEFAULT_ENV" && "$CONDA_DEFAULT_ENV" == "isnoda" ]]; then
+  echo "Conda environment 'isnoda' is active."
+else
+  echo "Conda environment 'isnoda' is not active, exiting script"
+  exit 1
+fi
 
 # Initial run, no previous days
 awsm_daily_airflow -c "${awsm_ini}" \
