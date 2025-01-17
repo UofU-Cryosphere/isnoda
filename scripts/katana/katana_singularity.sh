45fdc6968ba55ea7ebeb169b8d926ff6727578be
Joachim Meyer <jmeyer@boisestate.edu>
2025-01-17 09:36:20 -0700

    Scripts - katana - Convert to standalone execution wrapper
    Convert the singularity script to a standalone script that only needs
    update to the container image path.

--- scripts/katana/katana_singularity.sh
+++ scripts/katana/katana_singularity.sh
@@ -5,14 +5,14 @@
 # - ${1}: Basin setup directory with topo
 # - ${2}: Path to HRRR input data
 # - ${3}: Output folder for SMRF to ingest it
+# - ${4}: Full path to katana.ini
 
 ## Update these two variables with actual paths ##
 KATANA_IMAGE=/path/to/katana.sif
-KATANA_INI=/path/to/katana.ini
 ## ------------------------------ ##
 
 ml singularity
 
 export SINGULARITY_BIND="${1}:/data/topo,${2}:/data/input,${3}:/data/output"
 
-singularity exec ${KATANA_IMAGE} run_katana ${KATANA_INI}
+singularity exec ${KATANA_IMAGE} run_katana ${4}
