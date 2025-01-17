bfb5dedbebc4f5af85d04dca7749fd7d0e1c59d9
J. Michelle Hu <43484205+jmichellehu@users.noreply.github.com>
2025-01-17 14:19:12 -0700

    scripts - smeshr - minor cleanup
    Clean up echo statements for compactness.
    Correct extra underscore in missing lastday albedo echo.

--- scripts/smeshr/check_smeshr_outputs.sh
+++ scripts/smeshr/check_smeshr_outputs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Check outputs of SMESHR to determine entry points prepare_MODIS_HRRR_inputs.sh
-#
 # Arguments:
 #   ./check_smeshr_outputs.sh <BASIN> <WY>
 # Sample call:
@@ -12,10 +11,12 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: ./check_smeshr_outputs.sh <BASIN> <WY>"
     exit 1
 fi
+
 # Set variables
 BASIN=$1
 WY=$2
 
+# Global directories
 SMESHR_DIR=/uufs/chpc.utah.edu/common/home/skiles-group3/SMESHR/
 ALBEDO_DIR=/uufs/chpc.utah.edu/common/home/skiles-group3/MODIS_albedo/
 
@@ -32,7 +33,7 @@ if [ ! -e ${TCDC_out}/tcdc.MST.${LAST_DAY}.nc ]; then
     echo "*************************************************************"
     echo "Last day of WY missing: ${TCDC_out}/tcdc.MST.${LAST_DAY}.nc"
     echo "for HRRR cloud cover outputs <TCDC_out> for ${BASIN} ${WY}"
-    echo "Setting ENTRY_POINT to 0"
+    echo "Setting ENTRY_POINT=0"
     echo "*************************************************************"
     export ENTRY_POINT=0
     exit 1
@@ -43,18 +44,18 @@ elif [ ! -e ${DSWRF_DIR}/hrrr.${LAST_DAY}.dswrf.nc ]; then
     # check for the last processed day of this water year, if it exists
     LAST_PROCESSED_DAY=$(ls -1 ${DSWRF_DIR}/hrrr.${WY}*dswrf.nc | tail -n 1)
     echo "Last processed day of this WY ${WY} is ${LAST_PROCESSED_DAY}"
-    echo "Setting ENTRY_POINT to 1"
+    echo "Setting ENTRY_POINT=1"
     echo "*************************************************************"
     export ENTRY_POINT=1
     exit 1
 elif [ ! -e ${ALBEDO_DIR}/wy${WY}/westernUS_Terra_${LAST_DAY}_*albedo_${BASIN}_24.nc ]; then
     echo "*************************************************************"
-    echo "Last day of WY missing: ${ALBEDO_DIR}/wy${WY}/westernUS_Terra_${LAST_DAY}_*_albedo_${BASIN}_24.nc"
+    echo "Last day of WY missing: ${ALBEDO_DIR}/wy${WY}/westernUS_Terra_${LAST_DAY}_*albedo_${BASIN}_24.nc"
     echo " for MODIS albedo outputs <ALBEDO_DIR> for ${BASIN} ${WY}"
     # check for the last processed day of this water year, if it exists
     LAST_PROCESSED_DAY=$(ls -1 ${ALBEDO_DIR}/wy${WY}/westernUS_Terra_${WY}*_albedo_${BASIN}_24.nc | tail -n 1)
     echo "Last processed day of this WY ${WY} is ${LAST_PROCESSED_DAY}"
-    echo "Setting ENTRY_POINT to 2"
+    echo "Setting ENTRY_POINT=2"
     echo "*************************************************************"
     export ENTRY_POINT=2
     exit 1
@@ -64,24 +65,17 @@ elif [ ! -e ${NET_SOLAR_DIR}/net_dswrf.MST.${LAST_DAY}.nc ]; then
     # check for the last processed day of this water year, if it exists
     LAST_PROCESSED_DAY=$(ls -1 ${NET_SOLAR_DIR}/net_dswrf.MST.${WY}*.nc | tail -n 1)
     echo "Last processed day of this WY ${WY} is ${LAST_PROCESSED_DAY}"
-    echo "Setting ENTRY_POINT to 3"
+    echo "Setting ENTRY_POINT=3"
     echo "*************************************************************"
     export ENTRY_POINT=3
     exit 1
 else
     BASIN_CAP=$(echo $BASIN | tr '[:lower:]' '[:upper:]')
-    echo ""
-    echo "All SMESHR outputs (and HRRR-MODIS modified inputs) found for"
-    echo ""
-    echo "                 == ${BASIN_CAP} WY ${WY} =="
-    echo ""
-    echo "$(ls ${TCDC_out}/tcdc.MST.${LAST_DAY}.nc)"
-    echo "$(ls ${DSWRF_DIR}/hrrr.${LAST_DAY}.dswrf.nc)"
-    echo "$(ls ${ALBEDO_DIR}/wy${WY}/westernUS_Terra_${LAST_DAY}_*albedo_${BASIN}_24.nc)"
-    echo "$(ls ${NET_SOLAR_DIR}/net_dswrf.MST.${LAST_DAY}.nc)"
-    echo ""
-    echo ">>> Proceed to HRRR-MODIS first day runs <<<"
-    echo ""
-    echo ""
+    echo ; echo "All SMESHR outputs (and HRRR-MODIS modified inputs) found for" ; echo
+    echo "                 == ${BASIN_CAP} WY ${WY} ==" ; echo 
+    ls ${TCDC_out}/tcdc.MST.${LAST_DAY}.nc
+    ls ${DSWRF_DIR}/hrrr.${LAST_DAY}.dswrf.nc
+    ls ${ALBEDO_DIR}/wy${WY}/westernUS_Terra_${LAST_DAY}_*albedo_${BASIN}_24.nc
+    ls ${NET_SOLAR_DIR}/net_dswrf.MST.${LAST_DAY}.nc
+    echo ; echo ">>> Proceed to HRRR-MODIS first day runs <<<" ; echo ; echo
 fi
-# capitalize BASIN variable
