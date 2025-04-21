cb3906200e742616528076a7e5f5ee22c0f08b8a
J. Michelle Hu <43484205+jmichellehu@users.noreply.github.com>
2025-04-21 15:22:06 -0600

    scripts - smeshr - SPIReS
    Add scripts to handle SPIReS albedo and nomenclature

---
+++ scripts/smeshr/SPIReS/SPIReS_albedo_basin.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+
+# Script to extract albedo values for a model domain from SPIReS.
+# This prepares a NetCDF file with per grid cell albedo values for every
+# hour of one day. It can be directly used to calculate net solar
+# with incoming shortwave radiation.
+
+# Arguments:
+# ${1}: /path/to/SPIReS/inputs
+# ${2}: water year
+
+set -e
+
+# this is unnecessary, they are already in geotiff
+# extract_spires() {
+#   # command from UofU snow-rs package
+#   # https://github.com/UofU-Cryosphere/snow-rs
+#   # Converts the SPIReS matlab to GeoTiff files
+#   variable_from_spires --source-dir ${1} \
+#                       --water-year ${2} \
+#                       --variable albedo_observed_muZ
+
+#   if [ $? != 0 ]; then
+#     echo "Error extracting variable from SPIReS inputs"
+#     exit 1
+#   fi
+# }
+
+# Basin name and processing parameters
+export BASIN='blue'
+BASIN_EXTENT='391137.9 4356434 433937.9 4392434'
+RES=100
+EPSG='EPSG:32613'
+export BASIN_DOMAIN=" -t_srs $EPSG -tr $RES $RES -dstnodata 65535 -te $BASIN_EXTENT"
+SLURM_NTASKS=4
+
+export BASIN_NC="_${BASIN}.nc"
+export BASIN_ONE_DAY_SUFFIX="_24.nc"
+
+export CDO_call="cdo -O -z zip_4 -f nc4 -s"
+export PARALLEL_call="parallel --tagstring {#} --tag --line-buffer --jobs ${SLURM_NTASKS}"
+
+# Extracts the BASIN domain from the Western US SPIReS albedo GeoTiffs
+spires_basin() {
+  BASIN_TMP_CUBIC=${1/\.tif/_cubic_tmp.vrt}
+  BASIN_TMP_NN=${1/\.tif/_nn_tmp.vrt}
+  BASIN_TMP_MAX=${1/\.tif/_tmp_max.tif}
+  BASIN_TMP=${1/\.tif/_tmp.tif}
+  BASIN_TMP_NC=${1/\.tif/_tmp.nc}
+
+  # add catch for existing 24.nc files (final output), remove later as needed
+  if [[ -f "${1%.*}_${BASIN}${BASIN_ONE_DAY_SUFFIX}" ]]; then
+    return
+  else
+    echo "${1%.*}_${BASIN}${BASIN_ONE_DAY_SUFFIX}" DNE
+    FILTER_MATH="A*(A<=numpy.max(B)) + numpy.max(B)*(A>numpy.max(B))"
+    echo "gdalwarp -q -overwrite -multi \
+      -r cubic ${BASIN_DOMAIN} \
+      ${1} ${BASIN_TMP_CUBIC}"
+
+    # Use cubic resampling to smooth the image
+    gdalwarp -q -overwrite -multi \
+      -r cubic ${BASIN_DOMAIN} \
+      ${1} ${BASIN_TMP_CUBIC}
+
+    # Generate a nearest neighbor to get the max basin value as filter
+    gdalwarp -q -overwrite -multi \
+      ${BASIN_DOMAIN} \
+      ${1} ${BASIN_TMP_NN}
+
+    # Combine products, filter any values from cubic resampling higher
+    # than the max of nearest neighbor and set to that value
+    gdal_calc.py --overwrite --quiet --co COMPRESS=LZW \
+      -A ${BASIN_TMP_CUBIC} -B ${BASIN_TMP_NN} \
+      --calc="${FILTER_MATH}" \
+      --outfile=${BASIN_TMP_MAX}
+
+    # Add threshold enforcement of the values lower than the
+    # minimum of the nearest neighbor resampling and set to that min
+    gdal_calc.py --overwrite --quiet --co COMPRESS=LZW \
+      -A ${BASIN_TMP_MAX} -B ${BASIN_TMP_NN} \
+      --calc="A*(A>=numpy.min(B)) + numpy.min(B)*(A<numpy.min(B))" \
+      --outfile=${BASIN_TMP}
+
+    # Convert to NetCDF
+    gdalwarp -overwrite -multi -q \
+      -co FORMAT=NC4C -co WRITE_BOTTOMUP=NO \
+      ${BASIN_TMP} ${BASIN_TMP_NC}
+
+    if [ $? != 0 ]; then
+      printf "Error extracting domain from: \n   ${1}\n"
+      return 1
+    fi
+
+    # To extract date for format westernUS_YYYYMMDD_albedo.tif
+    date=$(date -d $(basename $1 | cut -d '_' -f2) +%Y-%m-%d)
+
+    # Set time and variable name in NetCDF
+    ${CDO_call} \
+      -chname,Band1,albedo \
+      -setdate,"${date}" \
+      -settunits,1hour \
+      ${BASIN_TMP_NC} ${1/\.tif/${BASIN_NC}}
+
+    if [ $? != 0 ]; then
+      echo "Error setting NetCDF variable name and time"
+      return 1
+    fi
+
+    printf "DONE: ${date}\n"
+
+    rm ${BASIN_TMP_CUBIC}
+    rm ${BASIN_TMP_NN}
+    rm ${BASIN_TMP}
+    rm ${BASIN_TMP_MAX}
+    rm ${BASIN_TMP_NC}
+  fi
+  }
+export -f spires_basin
+echo "Extracting SPIReS albedo"
+${PARALLEL_call} spires_basin {} ::: ${1}/wy${2}/westernUS*albedo.tif
+
+# Creates a NetCDF using the extracted model domain values and
+# populates every hour of one day with those.
+albedo_day() {
+  HOUR_FILE=${1/\.nc$//}
+
+  if [[ ! -f "${1}" ]]; then
+    printf "Error: Source NetCDF:  \n  ${1}\n  does not exist\n"
+    return 1
+  fi
+
+  # Duplicate the single day value for every hour
+  for hour in {1..23}; do
+    ${CDO_call} -shifttime,${hour}hour \
+      ${1} ${HOUR_FILE}_${hour}.nc
+  done
+
+  # Make on file with values for every full hour and given file
+  ${CDO_call} mergetime ${1} ${HOUR_FILE}*.nc ${1/\.nc/${BASIN_ONE_DAY_SUFFIX}}
+
+  if [ $? != 0 ]; then
+    printf "Error: Could not merge hours for:\n  ${1}\n"
+    return 1
+  fi
+
+  printf "*\n"
+
+  rm ${HOUR_FILE}*.nc
+  rm ${1}
+}
+
+export -f albedo_day
+echo "Creating NetCDFs"
+${PARALLEL_call} albedo_day ::: ${1}/wy${2}/*${BASIN_NC}
---
+++ scripts/smeshr/SPIReS/net_dswrf_for_topo_SPIReS.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+# Script to prepare net_solar.nc from HRRR data in expected format by iSnobal
+# Uses albedo from SPIReS.
+#
+# Script argument notes:
+# * Passed month should be based of a water year; October -> 0; September -> 11
+# * Paths need to be absolute for the parallel command to be able to find files
+#
+# Example call:
+#   ./net_HRRR_for_topo_SPIReS.sh <YYYY> <MM> <DSWRF_in> <SPIReS_in> <DSWRF_out>
+#   ./net_HRRR_for_topo_SPIReS.sh 2021 "0 1 2" /path/to/DSWRF /path/to/SPIReS /path/to/destination
+
+export OMP_NUM_THREADS=6
+export OMP_WAIT_POLICY=PASSIVE
+
+if [[ -z "$1" ]]; then
+  echo "Start year is required"
+  exit 1
+else
+  export WATER_YEAR=$((${1} - 1))
+fi
+export WATER_START_MONTH=10
+
+export DSWRF_IN=${3}
+export SPIReS_IN=${4}
+export DSWRF_OUT=${5}
+#export DAY_MST="${DSWRF_OUT}/net_dswrf.UTC"
+export BASIN=$(dirname ${DSWRF_OUT##*SMESHR/})
+export DAY_UTC="${DSWRF_OUT}/net_dswrf.UTC"
+
+export CDO_COMMAND='cdo -z zip4 -O -s'
+export HRRR_SELECT="-select,name=illumination_angle,DSWRF"
+export NET_MATH="net_solar=(1-albedo*0.0001)*illumination_angle*DSWRF;"
+
+# Merge by month to get one file per day starting at midnight UTC.
+# The 6-hour forecast requires to add the last day of the previous month
+function net_hrrr_for_month() {
+  pushd "${DSWRF_OUT}" || exit
+
+  CURRENT_MONTH=$(date -d "${WATER_START_MONTH}/01/${WATER_YEAR} + ${1} month")
+  if [[ $? != 0 ]]; then
+    exit 1
+  fi
+  MONTH=$(date -d "${CURRENT_MONTH}" +%m)
+  MONTH_SELECTOR=$(date -d "${CURRENT_MONTH}" +%Y%m)
+  LAST_DAY=$(date -d "${CURRENT_MONTH} - 1 day" +%Y%m%d)
+
+  echo "Processing: ${MONTH_SELECTOR}"
+
+  echo "  Merge HRRR month"
+  MONTH_FILE="${DSWRF_OUT}/dswrf.${MONTH_SELECTOR}.nc"
+  echo "${CDO_COMMAND} ${HRRR_SELECT} [ -mergetime ${DSWRF_IN}/*${LAST_DAY}* ${DSWRF_IN}/*${MONTH_SELECTOR}* ] ${MONTH_FILE}"
+  ${CDO_COMMAND} ${HRRR_SELECT} [ -mergetime ${DSWRF_IN}/*${LAST_DAY}* ${DSWRF_IN}/*${MONTH_SELECTOR}* ] ${MONTH_FILE}
+
+  if [[ $? != 0 ]]; then
+    echo "  ** Error merging HRRR month **"
+    exit 1
+  fi
+
+  echo "  Fetch Albedo"
+  SPIReS_MONTH="${SPIReS_IN}/*${MONTH_SELECTOR}*${BASIN}_24.nc"
+  SPIReS_MERGE="${DSWRF_OUT}/SPIReS.${MONTH_SELECTOR}.nc"
+  echo "${CDO_COMMAND} mergetime ${SPIReS_MONTH} ${SPIReS_MERGE}"
+  ${CDO_COMMAND} mergetime ${SPIReS_MONTH} ${SPIReS_MERGE}
+
+  if [[ $? != 0 ]]; then
+    echo "  ** Error getting SPIReS Albedo **"
+    exit 1
+  fi
+
+  echo "  Merge DSWRF and Albedo"
+  MERGE_FILE="${DSWRF_OUT}/HRRR_SPIReS.${MONTH_SELECTOR}.nc"
+  echo "$CDO_COMMAND merge -selmonth,${MONTH} ${SPIReS_MERGE} -selmonth,${MONTH} ${MONTH_FILE} ${MERGE_FILE}"
+  $CDO_COMMAND merge -selmonth,${MONTH} ${SPIReS_MERGE} -selmonth,${MONTH} ${MONTH_FILE} ${MERGE_FILE}
+
+  if [[ $? != 0 ]]; then
+    echo "  ** Error merging SPIReS and DSWRF **"
+    exit 1
+  fi
+
+  echo "  Calculate Net Solar HRRR SPIReS"
+  MONTH_CALC_FILE="${DSWRF_OUT}/net_HRRR.${MONTH_SELECTOR}.nc"
+  echo "${CDO_COMMAND} -aexpr,"${NET_MATH}" ${MERGE_FILE} ${MONTH_CALC_FILE}"
+  ${CDO_COMMAND} -aexpr,"${NET_MATH}" ${MERGE_FILE} ${MONTH_CALC_FILE}
+
+  if [[ $? != 0 ]]; then
+    echo "  ** Error calculating net DSWRF **"
+    exit 1
+  fi
+
+  echo "  Split by day UTC"
+  ${CDO_COMMAND} splitday ${MONTH_CALC_FILE} ${DAY_UTC}.${MONTH_SELECTOR}
+
+  if [[ $? != 0 ]]; then
+    echo "  ** Error processing ${MONTH_SELECTOR} **"
+  else
+    rm ${MONTH_FILE}
+    rm ${SPIReS_MERGE}
+    rm ${MERGE_FILE}
+  fi
+}
+
+export -f net_hrrr_for_month
+parallel --tagstring month-{} --line-buffer --jobs ${OMP_NUM_THREADS} net_hrrr_for_month ::: ${2}
+
