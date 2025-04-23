ca6565de31ae13a3fee50e0f35ffca8d0b4a8e1d
Joachim Meyer <jmeyer@boisestate.edu>
2025-04-23 09:07:00 -0600

    TopoSplit - save first round of work
    This starts a new approach to downscaling HRRR shortwave radiation that
    splits the incoming by direct and diffuse before applying terrain and
    atmospheric factors.
    
    Co-authored-by: arobledano <alvaro_adk@hotmail.com>
    Co-authored-by: Matt Olson <matt.olson@uvu.edu>

---
+++ scripts/smeshr/toposplit/topo_split_batch.py
@@ -0,0 +1,17 @@
+import sys
+
+from topo_split import *
+
+topo_file = '/uufs/chpc.utah.edu/common/home/uvu-group1/olson/snow-data/isnobal-data/ERW_topo.nc'
+
+day = sys.argv[1]
+date = day.split('.')[-1]
+print(date)
+ghi_stack, dsw1_stack, dsw2_stack, dsw3_stack, k_stack, time_list = toposplit_for_day(
+    topo_file, day
+)
+write_nc(
+    ghi_stack, dsw1_stack, dsw2_stack, dsw3_stack, k_stack, time_list,
+    '/uufs/chpc.utah.edu/common/home/uvu-group1/olson/snow-data/hrrr_toposplit/toposplit_' + date + '.nc',
+    topo_file,
+)
